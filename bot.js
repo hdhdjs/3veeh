873c0e0ee0b1a1e9bb0c05c7376fff87ca423355
hdhdjs <43729658+hdhdjs@users.noreply.github.com>
2018-10-05 20:22:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -343,7 +343,7 @@ client.on('message', message => {
 });
 
 const adminprefix = "M";//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-const devs = ['473980734109909007'];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+const devs = ['436866648381849600'];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 client.on('message', message => {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
   var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
     if (!devs.includes(message.author.id)) return;//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
